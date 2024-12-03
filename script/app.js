e7d28453339b2fb2df0118f74f90f9cc50e78af6
MandoxaElemental <gprcards@gmail.com>
2024-12-03 14:36:34 -0800

    now need to work on responsiveness (plus maybe some wicked if I have time...)

--- script/app.js
+++ script/app.js
@@ -30,19 +30,19 @@ function getData(){
 getData();
 
 getData().then(data => {
-    workHour.innerText = data[0].timeframes.weekly.current + 'hrs';
-    workPrev.innerText = yesterday + data[0].timeframes.weekly.previous + 'hrs';
-    playHour.innerText = data[1].timeframes.weekly.current + 'hrs';
-    playPrev.innerText = yesterday + data[1].timeframes.weekly.previous + 'hrs';
-    studyHour.innerText = data[2].timeframes.weekly.current + 'hrs';
-    studyPrev.innerText = yesterday + data[2].timeframes.weekly.previous + 'hrs';
-    exerciseHour.innerText = data[3].timeframes.weekly.current + 'hrs';
-    exercisePrev.innerText = yesterday + data[3].timeframes.weekly.previous + 'hrs';
-    socialHour.innerText = data[4].timeframes.weekly.current + 'hrs';
-    socialPrev.innerText = yesterday + data[4].timeframes.weekly.previous + 'hrs';
-    selfCareHour.innerText = data[5].timeframes.weekly.current + 'hrs';
-    selfCarePrev.innerText = yesterday + data[5].timeframes.weekly.previous + 'hrs';
-});
+        workHour.innerText = data[0].timeframes.weekly.current + 'hrs';
+        workPrev.innerText = lastWeek + data[0].timeframes.weekly.previous + 'hrs';
+        playHour.innerText = data[1].timeframes.weekly.current + 'hrs';
+        playPrev.innerText = lastWeek + data[1].timeframes.weekly.previous + 'hrs';
+        studyHour.innerText = data[2].timeframes.weekly.current + 'hrs';
+        studyPrev.innerText = lastWeek + data[2].timeframes.weekly.previous + 'hrs';
+        exerciseHour.innerText = data[3].timeframes.weekly.current + 'hrs';
+        exercisePrev.innerText = lastWeek + data[3].timeframes.weekly.previous + 'hrs';
+        socialHour.innerText = data[4].timeframes.weekly.current + 'hrs';
+        socialPrev.innerText = lastWeek + data[4].timeframes.weekly.previous + 'hrs';
+        selfCareHour.innerText = data[5].timeframes.weekly.current + 'hrs';
+        selfCarePrev.innerText = lastWeek + data[5].timeframes.weekly.previous + 'hrs';
+    });
 
 daily.addEventListener('click', function(){
     getData().then(data => {
@@ -66,17 +66,17 @@ daily.addEventListener('click', function(){
 weekly.addEventListener('click', function(){
     getData().then(data => {
         workHour.innerText = data[0].timeframes.weekly.current + 'hrs';
-        workPrev.innerText = yesterday + data[0].timeframes.weekly.previous + 'hrs';
+        workPrev.innerText = lastWeek + data[0].timeframes.weekly.previous + 'hrs';
         playHour.innerText = data[1].timeframes.weekly.current + 'hrs';
-        playPrev.innerText = yesterday + data[1].timeframes.weekly.previous + 'hrs';
+        playPrev.innerText = lastWeek + data[1].timeframes.weekly.previous + 'hrs';
         studyHour.innerText = data[2].timeframes.weekly.current + 'hrs';
-        studyPrev.innerText = yesterday + data[2].timeframes.weekly.previous + 'hrs';
+        studyPrev.innerText = lastWeek + data[2].timeframes.weekly.previous + 'hrs';
         exerciseHour.innerText = data[3].timeframes.weekly.current + 'hrs';
-        exercisePrev.innerText = yesterday + data[3].timeframes.weekly.previous + 'hrs';
+        exercisePrev.innerText = lastWeek + data[3].timeframes.weekly.previous + 'hrs';
         socialHour.innerText = data[4].timeframes.weekly.current + 'hrs';
-        socialPrev.innerText = yesterday + data[4].timeframes.weekly.previous + 'hrs';
+        socialPrev.innerText = lastWeek + data[4].timeframes.weekly.previous + 'hrs';
         selfCareHour.innerText = data[5].timeframes.weekly.current + 'hrs';
-        selfCarePrev.innerText = yesterday + data[5].timeframes.weekly.previous + 'hrs';
+        selfCarePrev.innerText = lastWeek + data[5].timeframes.weekly.previous + 'hrs';
     });
     daily.className = 'notSelected';
     weekly.className = 'selected';
@@ -85,17 +85,17 @@ weekly.addEventListener('click', function(){
 monthly.addEventListener('click', function(){
     getData().then(data => {
         workHour.innerText = data[0].timeframes.monthly.current + 'hrs';
-        workPrev.innerText = yesterday + data[0].timeframes.monthly.previous + 'hrs';
+        workPrev.innerText = lastMonth + data[0].timeframes.monthly.previous + 'hrs';
         playHour.innerText = data[1].timeframes.monthly.current + 'hrs';
-        playPrev.innerText = yesterday + data[1].timeframes.monthly.previous + 'hrs';
+        playPrev.innerText = lastMonth + data[1].timeframes.monthly.previous + 'hrs';
         studyHour.innerText = data[2].timeframes.monthly.current + 'hr';
-        studyPrev.innerText = yesterday + data[2].timeframes.monthly.previous + 'hrs';
+        studyPrev.innerText = lastMonth + data[2].timeframes.monthly.previous + 'hrs';
         exerciseHour.innerText = data[3].timeframes.monthly.current + 'hrs';
-        exercisePrev.innerText = yesterday + data[3].timeframes.monthly.previous + 'hrs';
+        exercisePrev.innerText = lastMonth + data[3].timeframes.monthly.previous + 'hrs';
         socialHour.innerText = data[4].timeframes.monthly.current + 'hrs';
-        socialPrev.innerText = yesterday + data[4].timeframes.monthly.previous + 'hrs';
+        socialPrev.innerText = lastMonth + data[4].timeframes.monthly.previous + 'hrs';
         selfCareHour.innerText = data[5].timeframes.monthly.current + 'hr';
-        selfCarePrev.innerText = yesterday + data[5].timeframes.monthly.previous + 'hrs';
+        selfCarePrev.innerText = lastMonth + data[5].timeframes.monthly.previous + 'hrs';
     });
     daily.className = 'notSelected';
     weekly.className = 'notSelected';
